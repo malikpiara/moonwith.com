79d3ea2db04f393ef6f9e29fa8480ee4751fc8ab
Malik Piara <malikpiara@gmail.com>
2023-11-02 00:16:25 +0100

    removed underline decoration from blog post titles.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="prose dark:prose-invert">
       {allPosts.map((post) => (
         <article key={post._id}>
-          <Link href={post.slug}>
+          <Link className="no-underline" href={post.slug}>
             <h2>{post.title}</h2>
           </Link>
           {post.description && <p>{post.description}</p>}
