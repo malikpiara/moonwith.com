63149d106be0602e386b38b8d33e1b459a078bf7
Malik Piara <malikpiara@gmail.com>
2023-11-02 00:01:52 +0100

    Changed background color for both themes.

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -15,7 +15,7 @@ export function ModeToggle() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          strokeWidth={1.5}
+          strokeWidth={2}
           stroke="currentColor"
           className="w-4 h-4">
           <path
