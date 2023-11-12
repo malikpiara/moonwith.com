261a243167ec58bb7672c4ebe34a7cc4f3b4ffe8
Malik Piara <malikpiara@gmail.com>
2023-11-12 16:57:57 +0100

    Removed subtitle from blog post pages.

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -52,11 +52,11 @@ export default async function PostPage({ params }: PostProps) {
   return (
     <article className="py-6 prose dark:prose-invert">
       <h1 className="mb-2">{post.title}</h1>
-      {post.description && (
+      {/* {post.description && (
         <p className="text-xl mt-0 text-slate-700 dark:text-slate-200">
           {post.description}
         </p>
-      )}
+      )} */}
       <hr className="my-4" />
       <Mdx code={post.body.code} />
     </article>
