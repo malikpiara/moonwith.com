9b7e49ef474b584a352d7e7be9ca1fbe8d03112c
Malik Piara <malikpiara@gmail.com>
2023-11-23 16:12:51 +0100

    Slight tweaks to the moon animation.

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             Only you know who you can be
           </p>
           <div className='bg-primary h-96 flex border-8 border-secondary dark:border-primary rounded-2xl overflow-hidden max-sm:hidden'>
-            <div className='bg-primary h-[40rem] w-[40rem] m-auto block rounded-full shadow-[inset_21px_2px_70px_rgba(252,249,242)] animate-pulse blur-md  transition-all before:content-[""] relative -bottom-36' />
+            <div className='bg-primary h-[40rem] w-[40rem] m-auto block rounded-full shadow-[inset_21px_2px_70px_rgba(252,249,242)] animate-pulse blur-md  transition-all before:content-[""] relative -bottom-36 border-8 hover:scale-90 duration-150 ease-in hover:border-tertiary' />
           </div>
           <Analytics />
         </ThemeProvider>
