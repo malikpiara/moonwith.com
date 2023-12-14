68353ad8dc120eeb8cbc6de4da6f5a50596e334d
Malik Piara <malikpiara@gmail.com>
2023-12-14 19:26:09 +0100

    Exercises: Changed strategy to using buttons insead of inputs and checkboxes.

--- components/logicola/exercise.tsx
+++ components/logicola/exercise.tsx
@@ -11,73 +11,51 @@ const Exercise = () => {
             <div>Not both A and B</div>
           </div>
 
-          <div className='flex items-center ps-4 border border-slate-200 rounded dark:border-slate-700'>
-            <input
-              id='bordered-checkbox-1'
-              type='checkbox'
-              value=''
-              name='bordered-checkbox'
-              className='w-4 h-4 text-green-600 bg-slate-100 border-slate-300 rounded focus:ring-green-500 dark:focus:ring-green-600 focus:ring-2'
-            />
-            <label
-              htmlFor='bordered-checkbox-1'
-              className='w-full py-4 ms-2 text-sm font-medium text-slate-900'
-            >
-              ~(A Ʌ B)
-            </label>
+          <div className='flex items-center border border-slate-200 rounded-lg'>
+            <button className='w-full ps-4 text-green-600  focus:ring-green-500 focus:ring-2'>
+              <div className='flex'>
+                <div className='py-4 ms-2 text-sm font-medium text-slate-600'>
+                  ~(A Ʌ B)
+                </div>
+              </div>
+            </button>
           </div>
-          <div className='flex items-center ps-4 border border-slate-200 rounded'>
-            <input
-              checked
-              id='bordered-checkbox-2'
-              type='checkbox'
-              value=''
-              name='bordered-checkbox'
-              className='w-4 h-4 text-green-600 bg-slate-100 border-slate-300 rounded focus:ring-green-500 dark:focus:ring-green-600'
-            />
-            <label
-              htmlFor='bordered-checkbox-2'
-              className='w-full py-4 ms-2 text-sm font-medium text-slate-900'
-            >
-              ~(A · B)
-            </label>
+
+          <div className='flex items-center border border-slate-200 rounded-lg'>
+            <button className='w-full ps-4 text-green-600  focus:ring-green-500 focus:ring-2'>
+              <div className='flex'>
+                <div className='py-4 ms-2 text-sm font-medium text-slate-600'>
+                  ~(A · B)
+                </div>
+              </div>
+            </button>
           </div>
-          <div className='flex items-center ps-4 border border-slate-200 rounded'>
-            <input
-              id='bordered-checkbox-2'
-              type='checkbox'
-              value=''
-              name='bordered-checkbox'
-              className='w-4 h-4 text-green-600 bg-slate-100 border-slate-300 rounded focus:ring-green-500 dark:focus:ring-green-600'
-            />
-            <label
-              htmlFor='bordered-checkbox-2'
-              className='w-full py-4 ms-2 text-sm font-medium text-slate-900'
-            >
-              (~A · ~B)
-            </label>
+
+          <div className='flex items-center border border-slate-200 rounded-lg'>
+            <button className='w-full ps-4 text-green-600  focus:ring-green-500 focus:ring-2'>
+              <div className='flex'>
+                <div className='py-4 ms-2 text-sm font-medium text-slate-600'>
+                  (~A · ~B)
+                </div>
+              </div>
+            </button>
           </div>
-          <div className='flex items-center ps-4 border border-slate-200 rounded'>
-            <input
-              id='bordered-checkbox-2'
-              type='checkbox'
-              value=''
-              name='bordered-checkbox'
-              className='w-4 h-4 text-green-600 bg-slate-100 border-slate-300 rounded focus:ring-green-500 dark:focus:ring-green-600'
-            />
-            <label
-              htmlFor='bordered-checkbox-2'
-              className='w-full py-4 ms-2 text-sm font-medium text-slate-900'
-            >
-              (~A Ʌ ~B)
-            </label>
+
+          <div className='flex items-center border border-slate-200 rounded-lg'>
+            <button className='w-full ps-4 text-green-600  focus:ring-green-500 focus:ring-2'>
+              <div className='flex'>
+                <div className='py-4 ms-2 text-sm font-medium text-slate-600'>
+                  (~A Ʌ ~B)
+                </div>
+              </div>
+            </button>
           </div>
         </div>
       </div>
       <hr className='h-px my-4 bg-slate-200 border-0'></hr>
       <button
         type='button'
-        className='text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
+        className='text-white bg-green-400 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
       >
         Check answer
       </button>
