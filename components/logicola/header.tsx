6c55fe2fa60d58ddc5bb269d20cc816d95f36f5f
Malik Piara <malikpiara@gmail.com>
2023-12-17 22:37:42 +0100

    Refactored header to make it more semantic and easier to edit.

--- components/logicola/header.tsx
+++ components/logicola/header.tsx
@@ -1,31 +1,45 @@
 import Image from 'next/image';
+import mascotPic from '@/public/mascot.png';
+
+const header = {
+  title: 'LogiCola for Everyone',
+  description:
+    'LogiCola helps you learn logic. This is a web version of the original software built by the late Professor Harry Gensler.',
+};
 
 export function Header() {
   return (
     <>
-      <section className='bg-white'>
+      <header className='bg-white'>
         <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-28'>
           <div className='flex gap-10'>
             <div>
-              <Image
-                src={'/mascot.png'}
-                alt='Mascot'
-                width={600}
-                height={600}
-              />
+              <Mascot />
             </div>
             <div className='mt-16'>
               <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-stone-900 md:text-5xl lg:text-5xl'>
-                LogiCola for Everyone
+                {header.title}
               </h1>
               <p className='text-lg font-normal text-stone-500 lg:text-xl'>
-                LogiCola helps you learn logic. This is a web version of the
-                original software built by the late Professor Harry Gensler.
+                {header.description}
               </p>
             </div>
           </div>
         </div>
-      </section>
+      </header>
     </>
+  );
+}
+
+export function Mascot() {
+  return (
+    <Image
+      src={mascotPic}
+      alt='Mascot'
+      width='600'
+      height='600'
+      priority
+      placeholder='empty'
+    />
   );
 }
