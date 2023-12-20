563572680bd77faf9485e995aeaef52950f2487c
Malik Piara <malikpiara@gmail.com>
2023-12-20 17:29:47 +0000

    Removed placeholder from homepage in order to launch logicola 0.1

--- app/(logicola)/logicola/page.tsx
+++ app/(logicola)/logicola/page.tsx
@@ -7,12 +7,11 @@ export default function Logicola() {
       <div className='flex flex-col m-auto'>
         <Header />
         <div className='p-4 w-full'>
-          <div className='p-4 border-2 border-stone-200 border-dashed rounded-lg dark:border-stone-700'>
+          {/* <div className='p-4 border-2 border-stone-200 border-dashed rounded-lg dark:border-stone-700'>
             <div className='flex items-center justify-center h-96 mb-4 rounded bg-stone-50 dark:bg-stone-800'>
               <div className='text-2xl text-stone-400 dark:text-stone-500' />
             </div>
-          </div>
-          I
+          </div> */}
         </div>
       </div>
     </>
