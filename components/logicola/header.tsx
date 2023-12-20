7e63e42c591ca845777a949e0ccc40ed6e5c1074
Malik Piara <malikpiara@gmail.com>
2023-12-20 18:00:12 +0000

    Made homepage header for logicola mobile responsive.

--- components/logicola/header.tsx
+++ components/logicola/header.tsx
@@ -12,15 +12,15 @@ export function Header() {
     <>
       <header className='bg-white'>
         <div className='py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-28'>
-          <div className='flex gap-10'>
+          <div className='flex sm:gap-10 flex-col sm:flex-row'>
             <div>
               <Mascot />
             </div>
-            <div className='mt-16'>
-              <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-logicolaPrimary md:text-5xl lg:text-5xl'>
+            <div className='mt-4 sm:mt-16'>
+              <h1 className='text-center sm:text-left mb-4 text-4xl font-extrabold tracking-tight leading-none text-logicolaPrimary md:text-5xl lg:text-5xl'>
                 {header.title}
               </h1>
-              <p className='text-lg font-normal text-stone-500 lg:text-xl'>
+              <p className='text-center sm:text-left text-lg font-normal text-stone-500 lg:text-xl'>
                 {header.description}
               </p>
             </div>
