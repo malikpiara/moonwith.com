73981a77722deff5a9e0d196b29215bd572242d5
Malik Piara <malikpiara@gmail.com>
2023-12-24 17:03:48 +0000

    Casa Piara hello world.

---
+++ app/(casapiara)/casapiara/page.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+export default function Logicola() {
+  return (
+    <>
+      <div className='flex m-auto h-screen'>
+        <div className='p-4 w-full flex'>
+          <h1>Malik</h1>
+        </div>
+        <div className='p-4 w-full flex'>
+          <h1>Sarah</h1>
+        </div>
+        <div className='p-4 w-full flex'>
+          <h1>Paty</h1>
+        </div>
+        <div className='p-4 w-full flex'>
+          <h1>Soraya</h1>
+        </div>
+      </div>
+    </>
+  );
+}
