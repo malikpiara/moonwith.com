df13931c2eb0953a9d7babffd0d380882d82593b
Malik Piara <malikpiara@gmail.com>
2024-01-15 21:22:59 +0100

    Made Moonwith navbar source code less redundant and easier to maintain.

--- components/moonwith/header.tsx
+++ components/moonwith/header.tsx
@@ -1,51 +1,40 @@
 import Link from 'next/link';
 import { ModeToggle } from '../mode-toggle';
 
+interface NavItem {
+  label: string;
+  destination: string;
+}
+
 /**
- * This component displays a navbar.
+ * This component displays a navbar for Moonwith's main website.
+ * To add new items to the navigation, add the items to the items constant.
  */
 export function Header() {
+  const items: NavItem[] = [
+    { label: 'Home', destination: '/' },
+    { label: 'About', destination: '/about' },
+    { label: 'Work', destination: '/projects' },
+    { label: 'Software', destination: '/dashboard' },
+    { label: 'Friends', destination: '/friends' },
+    { label: 'Get in Touch', destination: '/contact' },
+  ];
   return (
     <header className='flex bg-tertiary w-full absolute left-0 top-0 flex-wrap opacity-80 text-primary'>
       <div className='flex items-center justify-between'>
         <ModeToggle />
         <nav className='ml-auto font-medium space-x-1'>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/'
-          >
-            Home
-          </Link>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/about'
-          >
-            About
-          </Link>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/projects'
-          >
-            Work
-          </Link>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/dashboard'
-          >
-            Software
-          </Link>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/friends'
-          >
-            Friends
-          </Link>
-          <Link
-            className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
-            href='/contact'
-          >
-            Get in touch
-          </Link>
+          {items.map((item: NavItem) => {
+            return (
+              <Link
+                key={item.label}
+                className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
+                href={item.destination}
+              >
+                {item.label}
+              </Link>
+            );
+          })}
         </nav>
       </div>
     </header>
