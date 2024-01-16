bc5619c805eb38143350a99e205d406806bbedba
Malik Piara <malikpiara@gmail.com>
2024-01-16 02:31:18 +0100

    Made font for blog posts slightly larger on desktop.

--- app/(moonwith)/posts/[...slug]/page.tsx
+++ app/(moonwith)/posts/[...slug]/page.tsx
@@ -50,7 +50,7 @@ export default async function PostPage({ params }: PostProps) {
   }
 
   return (
-    <article className='py-6 prose dark:prose-invert dark:text-gray text-lg'>
+    <article className='py-6 prose dark:prose-invert dark:text-gray text-lg md:text-[1.2rem]'>
       <h1 className='mb-2 text-primary dark:text-secondary'>{post.title}</h1>
       {/* {post.description && (
         <p className="text-xl mt-0 text-stone-700 dark:text-stone-200">
--- components/moonwith/sidenote.tsx
+++ components/moonwith/sidenote.tsx
@@ -1,6 +1,6 @@
 export function Sidenote({ content = 'This is a sidenote.' }: any) {
   return (
-    <aside className='relative float-right -mr-96 right-20 w-60 border-blue-500 border-l-2 pl-2 text-sm hover:bg-yellow-100 hover:cursor-default transition-all duration-300 max-sm:hidden'>
+    <aside className='relative float-right -mr-96 right-20 w-60 border-blue-500 border-l-2 pl-2 text-sm hover:bg-yellow-100 hover:cursor-default transition-all duration-300 max-lg:hidden'>
       {content}
     </aside>
   );
