060008f57c6e1c495972b75b343258d2a341b970
Malik <malik@hey.com>
2024-04-18 14:13:52 +0200

    Changed deisng of navigation on desktop.

--- components/moonwith/header.tsx
+++ components/moonwith/header.tsx
@@ -114,15 +114,25 @@ export function Header() {
         </div>
       </nav>
 
-      <header className='max-sm:hidden flex w-full absolute left-0 top-2 flex-wrap opacity-80 text-primary dark:text-gray'>
-        <div className='flex items-center justify-between'>
+      <header className='max-sm:hidden flex w-full absolute left-0 top-2 flex-wrap justify-between text-primary dark:text-gray'>
+        <div className='flex items-center'>
           <ModeToggle />
-          <nav className='ml-auto font-semibold space-x-1'>
+          <Link
+            className='flex text-md p-2 transition-all duration-300 hover:opacity-100'
+            href={'/'}
+          >
+            <span className='font-semibold tracking-wide dark:text-gray flex'>
+              MOONWITH
+            </span>
+          </Link>
+        </div>
+        <div className='flex items-center justify-between'>
+          <nav className='ml-auto font-medium space-x-1'>
             {items.map((item: NavItem) => {
               return (
                 <Link
                   key={item.label}
-                  className='opacity-80 p-2 transition-all duration-300 hover:opacity-100'
+                  className='text-md p-2 transition-all duration-300 hover:opacity-100'
                   href={item.destination}
                 >
                   {item.label}
