87a29b3fe752f5ebcabda3083239c92bf92c2120
Malik Piara <malik@hey.com>
2025-08-16 18:55:45 +0100

    tweaked bg opacity for sidenote.

--- components/moonwith/sidenote.tsx
+++ components/moonwith/sidenote.tsx
@@ -1,6 +1,6 @@
 export function Sidenote({ content = 'This is a sidenote.' }: any) {
   return (
-    <aside className='relative float-right -mr-96 right-20 w-60 border-[#6ab25d] border-l-2 pl-2 text-sm hover:bg-green-100 hover:cursor-default transition-all duration-300 max-lg:hidden'>
+    <aside className='relative float-right -mr-96 right-20 w-60 border-[#6ab25d] border-l-2 pl-2 text-sm hover:bg-green-100 dark:bg-opacity-30 hover:cursor-default transition-all duration-300 max-lg:hidden'>
       {content}
     </aside>
   );
