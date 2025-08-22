59ae9be8a0236d4f4133a0cf3a4ba21cc10438d1
Malik Piara <malik@hey.com>
2025-08-22 19:54:02 +0100

    Reverted changed in r2 incremental caching setup and changed module resolution handling.

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,5 @@
 import { defineCloudflareConfig } from '@opennextjs/cloudflare';
-import r2IncrementalCache from '@opennextjs/cloudflare/dist/api/overrides/incremental-cache/r2-incremental-cache';
+import r2IncrementalCache from '@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache';
 
 export default defineCloudflareConfig({
   incrementalCache: r2IncrementalCache,
