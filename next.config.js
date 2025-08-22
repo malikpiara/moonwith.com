04006aaae2c66da587be3d49bb1693f7f949a0bd
Malik Piara <malik@hey.com>
2025-08-22 14:16:03 +0100

    Changed app config to support deployment to Cloudflare.

--- next.config.js
+++ next.config.js
@@ -19,4 +19,7 @@ const nextConfig = {
   }
 };
 
+//const { initOpenNextCloudflareForDev } = require("@opennextjs/cloudflare");
+//initOpenNextCloudflareForDev();
+
 module.exports = withContentCollections(nextConfig);
